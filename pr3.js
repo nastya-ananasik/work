f5f0338fb3b1715925333d53e182258da6c03350
nastya-ananasik <nastya.ananasiko9@gmail.com>
2025-03-13 20:02:50 +0200

    Add files via upload

---
+++ pr3.js
@@ -0,0 +1,78 @@
+// 1 
+// let num1 = parseFloat(prompt("Введіть перше число:"));
+// let num2 = parseFloat(prompt("Введіть друге число:"));
+// console.log(`
+// Результати:
+// Додавання: ${num1 + num2}
+// Віднімання: ${num1 - num2}
+// Множення: ${num1 * num2}
+// Ділення: ${num1 / num2}
+// Залишок від ділення: ${num1 % num2}
+// `);
+
+
+// 2 
+// let age = parseInt(prompt("Введіть ваш вік:"));
+// console.log(`Ви повнолітній: ${age >= 18 ? 'true' : 'false'}`);
+
+
+// 3 
+// let isAbonement = prompt("У вас є абонемент? (true/false):");
+// let money = parseInt(prompt("Скільки грошей на рахунку?"));
+// if (isAbonement == "true" || money >= 100)
+//     console.log("Доступ дозволено");
+// else
+//     console.log("Доступ заборонено");
+
+
+// 4 
+// let number = parseInt(prompt("Введіть число:"));
+// console.log(`Число ${number % 2 ? 'непарне' : 'парне'}`);
+
+
+// 5 
+// let money = parseInt(prompt("Ваш початковий баланс:"));
+// console.log("Поповнено на 500 грн");
+// console.log("Знято 200 грн");
+// money = money + 500 - 200;
+// console.log(`Нараховано 5%: ${money * 5 / 100} грн`);
+// console.log(`Підсумковий баланс: ${money + (money * 5 / 100)} грн`);
+
+
+// 6 
+// let num = parseInt(prompt("Введіть число (1-7):"));
+// switch (num) {
+//     case 1:
+//         console.log("Це понеділок");
+//         break;
+//     case 2:
+//         console.log("Це вівторок");
+//         break;
+//     case 3:
+//         console.log("Це середа");
+//         break;
+//     case 4:
+//         console.log("Це четверг");
+//         break;
+//     case 5:
+//         console.log("Це п'ятниця");
+//         break;
+//     case 6:
+//         console.log("Це субота");
+//         break;
+//     case 7:
+//         console.log("Це неділя");
+//         break;
+// }
+
+
+// 7 
+// let num1 = parseInt(prompt("Введіть перше число:"));
+// let num2 = parseInt(prompt("Введіть друге число:"));
+// console.log(`
+// Результати:
+// ${num1} & ${num2} = ${num1 & num2}
+// ${num1} | ${num2} = ${num1 | num2}
+// ${num1} ^ ${num2} = ${num1 ^ num2}
+// ${num1} << ${num2} = ${num1 << num2}
+// ${num1} >> ${num2} = ${num1 >> num2}`);
