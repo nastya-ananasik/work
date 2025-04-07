871ef22ddfb4c1d2cc6063eb840a3bccab071d93
nastya-ananasik <nastya.ananasiko9@gmail.com>
2025-04-07 17:20:39 +0300

    Add files via upload

---
+++ pr4.js
@@ -0,0 +1,29 @@
+// function calculateRectangleArea(width, height) {
+//     return width * height;
+// }
+// console.log(calculateRectangleArea(5, 10));
+
+
+// function isEven(number) {
+//     return number % 2 == 0;
+// }
+// console.log(isEven(4));
+// console.log(isEven(7));
+
+
+// function greetUser(name) {
+//     console.log(`Привіт, ${name}!`);
+// }
+// greetUser("Олена");
+
+
+// function sumArray(array) {
+//     return array.reduce((sum, num) => sum + num, 0);
+// }
+// console.log(sumArray([1, 2, 3, 4]));
+
+
+// function maxOfThree(a, b, c) {
+//     return Math.max(...[a, b, c]);
+// }
+// console.log(maxOfThree(5, 9, 2));
